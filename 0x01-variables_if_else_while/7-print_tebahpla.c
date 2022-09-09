e8e3fe88ab644ca6211e44ee47ad8aa314d135b5
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-09-09 15:48:00 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'z'; lower >= 'a'; lower--)
+	{
+		putchar(lower);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
