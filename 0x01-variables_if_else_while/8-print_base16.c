9c07a2dfb4839ae70bb77763e12eb59051f59d17
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-09-09 15:59:29 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char lower;
+	
+	for (x = 0; x < 10; x++)
+	{
+		putchar((x % 10) + '0');
+	}
+
+	for (lower = 'a'; lower <= 'f'; lower++)
+	{
+		putchar(lower);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
