644a43235455917a7e754228f43e63aace03b871
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-09-12 07:56:13 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return 1;
+	}
+
+	else
+	{
+		return 0;
+	}
+}
