10f6817eaad9c67a98e832c19626ad0c3468662d
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-09-13 00:51:13 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for uppercase character
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' || c >= 'a') && (c <= 'Z' || c <= 'z'))
+	{
+		return (1);
+	}
+	
+	else
+	{
+		return (0);
+	}
+}
