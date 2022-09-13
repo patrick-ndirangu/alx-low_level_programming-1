768cf1c62c33b7b1f0ec8008501cebdc2fc86d64
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-09-13 01:23:50 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: checks the character
+ *
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	return n;
+}
