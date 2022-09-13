159e932e4623fdbe7b845d103cfbbb96ebe335c1
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-09-13 01:25:50 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 
 	if (n == 0)
@@ -26,5 +26,5 @@ int print_sign(int n)
 		return (-1);
 	}
 
-	return n;
+	return (n);
 }
