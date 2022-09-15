097ece2fe933b5f172dae592166d3865a7ade222
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-09-15 14:48:58 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - prints from 0 to 9
- *Description: Do not print 2 and 4
+ * Description: Do not print 2 and 4
  * Return: Always 0 (Success)
  */
 void print_most_numbers(void)
