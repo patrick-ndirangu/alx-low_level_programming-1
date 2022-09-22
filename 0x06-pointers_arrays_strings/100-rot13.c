4ab328c7107dc665ce5dc68f90c393dd96cda4d4
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-09-22 12:59:30 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13
+ * @str: parameter to check
+ *
+ * Return: str
+ */
+char *rot13(char *str)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
+
