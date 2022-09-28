38a86f4bf806ab8f87d4012ac9f86f6204b949a5
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-09-28 12:11:07 -0700

    Function that checks whether an integer is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**is_prime_number - returns 1 if input integer is a prime
+ * number, otherwise returns 0
+ * @n: parameter to check
+ *
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 2)
+	{
+		return (0);
+	}
+
+	return (primeno(n, 2));
+}
+
+/**
+ * primeno - returns 1 if input integer is a prime
+ * number, otherwise returns 0
+ * @a: same as n
+ * @b: iterates from 1 to n
+ *
+ * Return: 1 or 0
+ */
+int primeno(int a, int b)
+{
+	if (a == b)
+	{
+		return (1);
+	}
+
+	else if (a % b == 0)
+	{
+		return (0);
+	}
+
+	return (primeno(a, b + 1));
+}
