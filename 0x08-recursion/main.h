c4b07b237c663f05c93cb573ffbacb190daa9a1a
Patrick Ndirangu <patrickmaina364@gmail.com>
2022-09-28 12:10:15 -0700

    Modified header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt2(int i, int j);
+int is_prime_number(int n);
+int primeno(int a, int b);
 
 #endif /* MAIN_H */
